6bfc0bae18c492a6547a68f73ded7d1e4ef39dc4
Livin Nector <livinnector2001@gmail.com>
2025-01-21 20:54:30 +0530

    feature: added fold level function to make single level dict from md

--- src/md2json/md2json.py
+++ src/md2json/md2json.py
@@ -1,5 +1,39 @@
+from typing import Literal
+
 from marko import Markdown
 from marko.md_renderer import MarkdownRenderer
+
+
+def fold_level(content, level, return_type: Literal["list", "dict"] = "dict"):
+    """Takes a markdown content and returns a folded dict with one heading level."""
+    document = Markdown().parse(content)
+    heading = None
+    if return_type == "dict":
+        folded_blocks = {}
+    elif return_type == "list":
+        folded_blocks = []
+
+    for block in document.children:
+        if block.get_type() == "Heading" and block.level == level:
+            heading = block.children[0].children
+            if return_type == "dict":
+                folded_blocks[heading] = []
+                current_blocks = folded_blocks[heading]
+            elif return_type == "list":
+                folded_blocks.append([heading, []])
+                current_blocks = folded_blocks[-1][-1]
+        elif heading:
+            current_blocks.append(block)
+    renderer = MarkdownRenderer()
+    folded_blocks = [
+        (level_heading, "".join(renderer.render(block) for block in blocks))
+        for level_heading, blocks in (
+            folded_blocks.items() if return_type == "dict" else folded_blocks
+        )
+    ]
+    if return_type == "dict":
+        folded_blocks = dict(folded_blocks)
+    return folded_blocks
 
 
 def dictify(content):
