727ffb7bb0d94ad7c4f24caa472b92ac8f8ed2df
Livin Nector <livinnector2001@gmail.com>
2025-01-22 12:18:40 +0530

    rewrite create template using ProQ.to_file

--- src/proq/cli.py
+++ src/proq/cli.py
@@ -1,6 +1,10 @@
+import os
+from typing import Literal
+
 import fire
 
-from . import create, evaluate, export
+from . import evaluate, export
+from .core import ProQ
 
 
 class ProqCli:
@@ -10,9 +14,34 @@ class ProqCli:
     """
 
     def __init__(self) -> None:
-        self.create = create.generate_template
         self.evaluate = evaluate.evaluate_proq_files
         self.export = export.proq_export
+
+    def create(
+        self,
+        output_file: str,
+        lang: Literal["python", "java", "c"] = "python",
+        n_public: int = 5,
+        n_private: int = 5,
+    ):
+        """Creates an empty proq file template with the given configuation.
+
+        Args:
+            output_file (str): Output file name
+            lang (Literal["python","java","c"]) : Programming language used to
+                create automatic execute configs.
+                Possible values are python, java and c.
+            n_public (int) : Number of public test cases
+            n_private (int) : Number of private test cases
+        """
+        if os.path.isfile(output_file):
+            raise FileExistsError(
+                f"A file with the name '{output_file}' already exists."
+            )
+
+        ProQ.default_proq(
+            lang=lang.lower().strip(), n_public=n_public, n_private=n_private
+        ).to_file(output_file)
 
 
 def main():
